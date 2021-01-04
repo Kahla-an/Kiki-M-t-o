d9e589cab02c214462a14287514620aa830a1ccf
Cyril-Alienware M11x <brun.cyril@hotmail.fr>
2021-01-04 13:31:11 +0100

    Changement Projets et Test

--- KikiMétéo/KikiMétéo/main.c
+++ KikiMétéo/KikiMétéo/main.c
@@ -3,6 +3,9 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    //Les poulets, j'ai refait le projet paske j'ai fracassé le ubuntu. Du coup, essayez chez vous ce main.c Voir un
+    //peu ce que ca donne. Si ça marche surtout.
+
+    printf("Ça marche ?");
     return 0;
 }

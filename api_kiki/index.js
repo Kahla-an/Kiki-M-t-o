1194889e68022bf22d92bad007ef10b22d062ef5
Cyril Wsh <brun.cyril@hotmail.fr>
2021-02-08 19:48:02 +0100

    Mise en place requests

--- api_kiki/index.js
+++ api_kiki/index.js
@@ -1,3 +1,5 @@
+"use strict"
+
 //express initialization
 const { request, response } = require("express")
 const express = require("express")
@@ -7,15 +9,22 @@ app.use(express.json())
 
 //mysql initialization
 const mysql = require("mysql")
-
-
-//Database
 const DB = mysql.createConnection({
     host :"localhost",
     user: "cyril_user",
-    password: "cyril_user"
+    password: "cyril_user",
+    database:"Kiki_meteo"
 
 })
+const create_query = ``
+const update_query = ``
+const delete_query = ``
+const read_query = `SELECT id_sonde_fk AS "ID Sonde", nom_sonde AS "Nom Sonde",  date_enregistrement AS "Date Enregistrement", temperature AS "Température", humidite*100 AS "Taux d'Humidité" FROM releves INNER JOIN sondes ON releves.id_sonde_fk = sondes.id_sonde;`
+
+
+
+
+
 DB.connect(function(err){
     if(err){
         console.error('error connecting: '+ err.stack)
@@ -34,8 +43,8 @@ const HOST = 'localhost'
 
 //CREATE
 app.post('/api/create', (req, res) => {
-    console.log(req.body)
-    // db.insert(req.body)
+    
+    DB.insert(req.body)
     // res.send(req.body)
 })
 
